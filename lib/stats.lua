594e1aecd33a5593aedcd7a70f0779c827ca6ad7
Tim Menzies <tim.menzies@gmail.com>
2018-07-18 01:27:17 -0400

    Committed 07/18/18

--- lib/stats.lua
+++ lib/stats.lua
@@ -1,4 +1,4 @@
-require "Num"
+require "thing"
 
 local tests = {
   {2,	{"x1",	0.34,	 0.49,	 0.51,	  0.6},	
@@ -30,24 +30,32 @@ local function bigTest()
   return {3,  t1, t2, t3}
 end
 
-function bchop(t, x, lo, hi)
-  local lo,hi = lo or 1, hi or #t
-  while lo < hi do
-     local mid = math.floor((lo+hi) / 2)
-     if t[mid] < x then lo = mid+1 else hi = mid end end
-  return lo
-end
-
-function cliffsDelta(t1, t2, small)
-  local lt, gt = 0,0
-  table.sort(t2)
+function cliffsDelta(t1, t2, small,max)
+  local max = max or 1000
+  local small = small or 0.147
+  local n,lt,gt = 0,0,0
+  if #t1 > max then
+     t1 = slice(shuffle(t1), 1, max) end
+  if #t2 > max then
+     t2 = slice(shuffle(t2), 1, max) end
   for _,x in pairs(t1) do
-    pos  = bchop(t2, x)
-    gt = gt + #t2 - j - 11 
-    lt = lt + j end
-  return (abs(gt - lt) / (#t1 * #t2)) < (small or 0.38)
+    for _,y in pairs(t2) do
+      n = n + 1
+      if x > y then gt = gt + 1 end
+      if x < y then lt = lt + 1 end end end
+  return abs(gt - lt) / n < (small or 0.147) 
 end
 
+function cliffsDeltaOkay()
+  for i=10,1,-1 do
+    local a,b={},{}
+    for _=1,10000 do a[#a+1] = rand() end
+    for j,x in pairs(a) do b[j] = x*i/10 end
+    print(i, cliffsDelta(a,b))
+  end
+end
+
+cliffsDeltaOkay()
 
 -- def cliffsDelta(lst1,lst2,
 --                 dull = [0.147, # small
@@ -105,156 +113,156 @@ end
 
 function Pairs._lt(a,b) return a.stats.mu < b.stats.mu end 
 
-function scottknott(data,cohen, small, epsilon)
-  cohen = cohen or 0.3
-  local all = {}
-  for _,one in pairs(data) do all:incs(one.all) end
-  small = small or all.stats:sd()*cohen
-  local function div(1, #data, all)
-    local left,right = Num:new(), Num:new():incs(all)    
-
-  local function split(data, lo, ho)
-  all  = reduce(lambda x,y:x+y,data)
-  same = lambda l,r: abs(l.median() - r.median()) <= all.s()*cohen
-  if useA12:
-    same = lambda l, r:   not different(l.all,r.all) 
-  big  = lambda    n: n > small
-  return rdiv(data,all,minMu,big,same,epsilon)
-
-def rdiv(data,  # a list of class Nums
-         all,   # all the data combined into one num
-         div,   # function: find the best split
-         big,   # function: rejects small splits
-         same, # function: rejects similar splits
-         epsilon): # small enough to split two parts
-  """Looks for ways to split sorted data, 
-  Recurses into each split. Assigns a 'rank' number
-  to all the leaf splits found in this way. 
-  """
-  def recurse(parts,all,rank=0):
-    "Split, then recurse on each part."
-    cut,left,right = maybeIgnore(div(parts,all,big,epsilon),
-                                 same,parts)
-    if cut: 
-      # if cut, rank "right" higher than "left"
-      rank = recurse(parts[:cut],left,rank) + 1
-      rank = recurse(parts[cut:],right,rank)
-    else: 
-      # if no cut, then all get same rank
-      for part in parts: 
-        part.rank = rank
-    return rank
-  recurse(sorted(data),all)
-  return data
-
-def maybeIgnore((cut,left,right), same,parts):
-  if cut:
-    if same(sum(parts[:cut],Num('upto')),
-            sum(parts[cut:],Num('above'))):    
-      cut = left = right = None
-  return cut,left,right
-
-def minMu(parts,all,big,epsilon):
-  """Find a cut in the parts that maximizes
-  the expected value of the difference in
-  the mean before and after the cut.
-  Reject splits that are insignificantly
-  different or that generate very small subsets.
-  """
-  cut,left,right = None,None,None
-  before, mu     =  0, all.mu
-  for i,l,r in leftRight(parts,epsilon):
-    if big(l.n) and big(r.n):
-      n   = all.n * 1.0
-      now = l.n/n*(mu- l.mu)**2 + r.n/n*(mu- r.mu)**2  
-      if now > before:
-        before,cut,left,right = now,i,l,r
-  return cut,left,right
-
-def leftRight(parts,epsilon=The.epsilon):
-  """Iterator. For all items in 'parts',
-  return everything to the left and everything
-  from here to the end. For reasons of
-  efficiency, take a first pass over the data
-  to pre-compute and cache right-hand-sides
-  """
-  rights = {}
-  n = j = len(parts) - 1
-  while j > 0:
-    rights[j] = parts[j]
-    if j < n: rights[j] += rights[j+1]
-    j -=1
-  left = parts[0]
-  for i,one in enumerate(parts):
-    if i> 0:
-      if parts[i]._median - parts[i-1]._median > epsilon:
-        yield i,left,rights[i]
-      left += one
-"""
-
-
-## Putting it All Together
-
-Driver for the demos:
-
-"""
-def rdivDemo(data):
-  def zzz(x):
-    return int(100 * (x - lo) / (hi - lo + 0.00001))
-  data = map(lambda lst:Num(lst[0],lst[1:]),
-             data)
-  print("")
-  ranks=[]
-  for x in scottknott(data,useA12=True):
-    ranks += [(x.rank,x.median(),x)]
-  all=[]
-  for _,__,x in sorted(ranks): all += x.all
-  all = sorted(all)
-  lo, hi = all[0], all[-1]
-  line = "----------------------------------------------------"
-  last = None
-  print(('%4s , %12s ,    %s   , %4s ' % \
-               ('rank', 'name', 'med', 'iqr'))+ "\n"+ line)
-  for _,__,x in sorted(ranks):
-    q1,q2,q3 = x.quartiles()
-    print(('%4s , %12s ,    %4s  ,  %4s ' % \
-                 (x.rank+1, x.name, q2, q3 - q1))  + \
-              xtile(x.all,lo=lo,hi=hi,width=30,show="%5.2f"))
-    last = x.rank 
-
-def _rdivs():
-  seed(1)
-  rdiv0();  rdiv1(); rdiv2(); rdiv3(); 
-  rdiv5(); rdiv6(); print("###"); rdiv7()
-
-####################################
-
-def thing(x):
-    "Numbers become numbers; every other x is a symbol."
-    try: return int(x)
-    except ValueError:
-        try: return float(x)
-        except ValueError:
-            return x
-          
-def main():
-  log=None
-  all={}
-  now=[]
-  for line in sys.stdin:
-    for word in line.split():
-      word = thing(word)
-      if isinstance(word,str):
-        now = all[word] = all.get(word,[])
-      else:
-        now += [word]
-  rdivDemo( [ [k] + v for k,v in all.items() ] ) 
-
-  
-if args.demo:
-  _rdivs()
-else:
-  main()
-  
+-- function scottknott(data,cohen, small, epsilon)
+--   cohen = cohen or 0.3
+--   local all = {}
+--   for _,one in pairs(data) do all:incs(one.all) end
+--   small = small or all.stats:sd()*cohen
+--   local function div(1, #data, all)
+--     local left,right = Num:new(), Num:new():incs(all)    
+-- 
+--   local function split(data, lo, ho)
+--   all  = reduce(lambda x,y:x+y,data)
+--   same = lambda l,r: abs(l.median() - r.median()) <= all.s()*cohen
+--   if useA12:
+--     same = lambda l, r:   not different(l.all,r.all) 
+--   big  = lambda    n: n > small
+--   return rdiv(data,all,minMu,big,same,epsilon)
+-- 
+-- def rdiv(data,  # a list of class Nums
+--          all,   # all the data combined into one num
+--          div,   # function: find the best split
+--          big,   # function: rejects small splits
+--          same, # function: rejects similar splits
+--          epsilon): # small enough to split two parts
+--   """Looks for ways to split sorted data, 
+--   Recurses into each split. Assigns a 'rank' number
+--   to all the leaf splits found in this way. 
+--   """
+--   def recurse(parts,all,rank=0):
+--     "Split, then recurse on each part."
+--     cut,left,right = maybeIgnore(div(parts,all,big,epsilon),
+--                                  same,parts)
+--     if cut: 
+--       # if cut, rank "right" higher than "left"
+--       rank = recurse(parts[:cut],left,rank) + 1
+--       rank = recurse(parts[cut:],right,rank)
+--     else: 
+--       # if no cut, then all get same rank
+--       for part in parts: 
+--         part.rank = rank
+--     return rank
+--   recurse(sorted(data),all)
+--   return data
+-- 
+-- def maybeIgnore((cut,left,right), same,parts):
+--   if cut:
+--     if same(sum(parts[:cut],Num('upto')),
+--             sum(parts[cut:],Num('above'))):    
+--       cut = left = right = None
+--   return cut,left,right
+-- 
+-- def minMu(parts,all,big,epsilon):
+--   """Find a cut in the parts that maximizes
+--   the expected value of the difference in
+--   the mean before and after the cut.
+--   Reject splits that are insignificantly
+--   different or that generate very small subsets.
+--   """
+--   cut,left,right = None,None,None
+--   before, mu     =  0, all.mu
+--   for i,l,r in leftRight(parts,epsilon):
+--     if big(l.n) and big(r.n):
+--       n   = all.n * 1.0
+--       now = l.n/n*(mu- l.mu)**2 + r.n/n*(mu- r.mu)**2  
+--       if now > before:
+--         before,cut,left,right = now,i,l,r
+--   return cut,left,right
+-- 
+-- def leftRight(parts,epsilon=The.epsilon):
+--   """Iterator. For all items in 'parts',
+--   return everything to the left and everything
+--   from here to the end. For reasons of
+--   efficiency, take a first pass over the data
+--   to pre-compute and cache right-hand-sides
+--   """
+--   rights = {}
+--   n = j = len(parts) - 1
+--   while j > 0:
+--     rights[j] = parts[j]
+--     if j < n: rights[j] += rights[j+1]
+--     j -=1
+--   left = parts[0]
+--   for i,one in enumerate(parts):
+--     if i> 0:
+--       if parts[i]._median - parts[i-1]._median > epsilon:
+--         yield i,left,rights[i]
+--       left += one
+-- """
+-- 
+-- 
+-- ## Putting it All Together
+-- 
+-- Driver for the demos:
+-- 
+-- """
+-- def rdivDemo(data):
+--   def zzz(x):
+--     return int(100 * (x - lo) / (hi - lo + 0.00001))
+--   data = map(lambda lst:Num(lst[0],lst[1:]),
+--              data)
+--   print("")
+--   ranks=[]
+--   for x in scottknott(data,useA12=True):
+--     ranks += [(x.rank,x.median(),x)]
+--   all=[]
+--   for _,__,x in sorted(ranks): all += x.all
+--   all = sorted(all)
+--   lo, hi = all[0], all[-1]
+--   line = "----------------------------------------------------"
+--   last = None
+--   print(('%4s , %12s ,    %s   , %4s ' % \
+--                ('rank', 'name', 'med', 'iqr'))+ "\n"+ line)
+--   for _,__,x in sorted(ranks):
+--     q1,q2,q3 = x.quartiles()
+--     print(('%4s , %12s ,    %4s  ,  %4s ' % \
+--                  (x.rank+1, x.name, q2, q3 - q1))  + \
+--               xtile(x.all,lo=lo,hi=hi,width=30,show="%5.2f"))
+--     last = x.rank 
+-- 
+-- def _rdivs():
+--   seed(1)
+--   rdiv0();  rdiv1(); rdiv2(); rdiv3(); 
+--   rdiv5(); rdiv6(); print("###"); rdiv7()
+-- 
+-- ####################################
+-- 
+-- def thing(x):
+--     "Numbers become numbers; every other x is a symbol."
+--     try: return int(x)
+--     except ValueError:
+--         try: return float(x)
+--         except ValueError:
+--             return x
+--           
+-- def main():
+--   log=None
+--   all={}
+--   now=[]
+--   for line in sys.stdin:
+--     for word in line.split():
+--       word = thing(word)
+--       if isinstance(word,str):
+--         now = all[word] = all.get(word,[])
+--       else:
+--         now += [word]
+--   rdivDemo( [ [k] + v for k,v in all.items() ] ) 
+-- 
+--   
+-- if args.demo:
+--   _rdivs()
+-- else:
+--   main()
+--   
 
 
