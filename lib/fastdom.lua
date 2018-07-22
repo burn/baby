a044f755f7831e471638787d2265b6ba7c1d66e3
Tim Menzies <tim.menzies@gmail.com>
2018-07-22 11:49:30 -0400

    HTML updates

---
+++ lib/fastdom.lua
@@ -0,0 +1,78 @@
+require "lib"
+
+-- Returns distance between rows, in objective space.
+local function dist(i, j,data)
+  local d,n,z = 0,The.zip,The.zip
+  for _,num  in pairs(data.y.nums) do
+      local a = i.cells[ num.pos ]
+      local b = j.cells[ num.pos ]
+      a = (a - num.lo) / (num.hi - num.lo + z)
+      b = (b - num.lo) / (num.hi - num.lo + z)
+      d = d + (a-b)^2
+      n = n + 1 end
+  return d^0.5 / n^0.5 
+end
+
+-- Returns  furthest poinf from i, in objective space.
+local function furthest(i, lst, data)
+  local most,out = -1,i
+  for _,j in pairs(lst) do
+    local d = dist(i,j, data)
+    if d > most then most,out = d,j end end
+  return out
+end
+
+-- Pick anything. Find the `y` furthest from anything.
+-- Find the `z` furthest from `y`. 
+-- Return `y,z`
+local function distantPoints(data,rows,y,z)
+  local x = any(rows)
+  y = y or furthest(x, rows, data)
+  z = z or furthest(y, rows, data)
+  return y,z
+end
+
+-- Top down bi-clustering of rows.
+-- Find two most distant points. Label them
+-- `bad` and `best` depending on who dominates wo.
+-- Divide rows into those that are nearest `bad` or `best,
+-- Recurse till too few rows. Then score each
+-- row bad to best.
+local function div(data,rows,few,        rank, bad, best)
+  rank = rank or 1
+  if #rows < few then
+    for _,one in pairs(rows) do
+      rank = rank + 1
+      one.dom = rank end
+  else
+    bad,best = distantPoints(data,rows,bad,best)
+    if bad:dom(best,data) then 
+      bad,best = best,bad end
+    local c  = dist(bad,best, data)
+    local c1 = c + The.dom.tiny
+    local tmp = {}
+    for pos,row in pairs(rows) do
+      local a = dist(bad, row, data)
+      local b = dist(best, row, data)
+      if a > c1 then 
+	 return div(data, rows, few,rank, row, bad) end
+      if b > c1 then 
+	 return div(data, rows, few,rank, row, best) end
+      local x = (a*a + c*c - b*b) / (2*c + The.zip)
+      tmp[ #tmp+1 ] = {x,row} 
+    end
+    tmp = sorted(tmp, function(x,y) return x[1] < y[1] end)
+    for i,one in pairs(tmp) do tmp[i] = one[2] end
+    local mid = int(#rows/2)
+    rank = div(data, slice(tmp,    1, mid),  few, rank)
+    rank = div(data, slice(tmp,mid+1, #tmp), few, rank) 
+  end
+  return rank 
+end
+
+local function fastdom(data,rows)
+  few = max(The.dom.few, (#rows)^The.dom.power)
+  div(data, rows, few) 
+end
+
+return fastdom
---
+++ lib/row.lua
@@ -0,0 +1,43 @@
+require "lib"
+
+Row = Any:new{cells, _dom, best=false}
+
+-------------------------------------------------
+-- ## Row Methods
+
+function Row:has(data, y,z)
+  local t={}
+  for _,head in pairs(data[y][z]) do
+    t[ head.txt ] = self.cells[head.pos] end
+  return t
+end
+
+
+-- ### Row:dom(row2:row, nums: list of Num): boolean
+-- Returns true if self dominates row2.
+-- Computed using the row cells found in `nums`
+-- and the Zilter continuous domination indicator
+-- (so should work for many more goals than just 2).
+function Row:dom(j, data) 
+  local nums = data.y.nums
+  local s1, s2, n, z = 0, 0, #nums, The.zip
+  for _,num in pairs(nums) do
+    local a = self.cells[ num.pos ]
+    local b =    j.cells[ num.pos ]
+    a       = (a - num.lo) / (num.hi - num.lo + z)
+    b       = (b - num.lo) / (num.hi - num.lo + z)
+    s1      = s1 - 10^(num.w * (a - b) / n)
+    s2      = s2 - 10^(num.w * (b - a) / n) end
+  return s1 / n < s2 / n 
+end
+
+-- ### Row:ndominates(d: data): integer
+-- Returns a count how how rows in `d` are domianted by self.
+function Row:ndom(data, others)
+  local n = 0
+  for _,row in pairs(others) do
+    if self:dom(row, data) then n=n+1 end end 
+  return n 
+end
+
+
