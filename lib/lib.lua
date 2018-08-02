0ee8e4f73e0ddd61d676c164aa94b0ed073f2b9d
Tim Menzies <tim.menzies@gmail.com>
2018-08-02 01:30:40 -0400

    Committed 08/02/18

--- lib/lib.lua
+++ lib/lib.lua
@@ -262,15 +262,16 @@ function lib.cols(t)
   for i,_ in pairs(t[1]) do w[i] = 0 end
   for _,line in pairs(t) do
     for j,cell in pairs(line) do 
-      w[j] = lib.max( w[j], #tostring(cell) ) end
+      w[j] = lib.max( w[j], #tostring(cell) ) end end
   for i,cell in pairs(w) do w[i] = "%"..w[i].."s" end 
   for _,line in pairs(t) do
     local txt,sep="",""
     for j,cell in pairs(line) do
       print(txt,sep)
       txt = txt .. sep .. lib.sprintf(w[j], cell)
-      sep = "," end  
-    print(txt) end end
+      sep = "," 
+    end  
+    print(txt) end 
 end
 
 -------------------------------------------------------------
